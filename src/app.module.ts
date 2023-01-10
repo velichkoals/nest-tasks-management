e21318e7b9cfc4ed93cb855aedd2352355d9c3e2
velichkoals <velichkoalsprog@gmail.com>
2023-01-10 09:10:55 +0200

    feat: configuration variables, env validation with joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,32 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthModule } from './auth/auth.module'
 import { Task } from './entities/task.entity'
 import { User } from './entities/user.entity'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import * as process from 'process'
+import { configValidationSchema } from './config.schema'
 
 @Module({
 	imports: [
 		TasksModule,
 		AuthModule,
-		TypeOrmModule.forRoot({
-			type: 'postgres',
-			host: 'localhost',
-			port: 5432,
-			username: 'postgres',
-			password: 'postgres',
-			database: 'task-management',
-			autoLoadEntities: true,
-			synchronize: true,
-			entities: [Task, User],
+		ConfigModule.forRoot({
+			envFilePath: [`.env.stage.${process.env.STAGE}`],
+			validationSchema: configValidationSchema,
+		}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: async (configService: ConfigService) => ({
+				type: 'postgres',
+				host: configService.get('DB_HOST'),
+				port: configService.get('DB_PORT'),
+				username: configService.get('DB_USERNAME'),
+				password: configService.get('DB_PASSWORD'),
+				database: configService.get('DB_DATABASE'),
+				autoLoadEntities: true,
+				synchronize: true,
+				entities: [Task, User],
+			}),
 		}),
 	],
 })
